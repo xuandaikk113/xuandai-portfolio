7b6cb7180c037e24b591c89c9228a3beb5a70832
xuandaikk113 <nguyenvoxuandaikk113@gmail.com>
2024-11-18 17:33:43 +0700

    1/3 project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Instroduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        Your work anniversary is a reminder that you’ve spent another year not
+        getting caught for internet shopping at work.
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
