465f9327b68a1e4a4539d0a3d32ec2e3f0a55f76
xuandaikk113 <nguyenvoxuandaikk113@gmail.com>
2024-11-20 10:21:02 +0700

    done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -68,4 +68,4 @@ const Experience = () => {
   );
 };
 
-export default SectionWrapper(Experience, "work");
+export default SectionWrapper(Experience, "experience");
--- src/constants/index.js
+++ src/constants/index.js
@@ -31,6 +31,10 @@ export const navLinks = [
         title: "About",
     },
     {
+        id: "experience",
+        title: "Experience",
+    },
+    {
         id: "work",
         title: "Work",
     },
